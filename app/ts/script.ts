e9a3f44ab36891f749d9916175190a5cee79c513
JuniorCoder <thomas08samuel@gmail.com>
2022-02-17 14:19:16 +0100

    ts-color-vars

--- app/ts/script.ts
+++ app/ts/script.ts
@@ -1,3 +1,12 @@
-const test: string = 'Hello World';
-
-console.log(test);
+const colors: Object = {
+    main: '#568FF0',
+    darkMain: '#2F3137',
+    blackMain: '#191A1D',
+    green: '#27FFA9',
+    red: '#FF0077',
+    yellow: '#FFCC00',
+    white: '#eeeeee',
+    black: '#1b1b1b',
+    fullWhite: '#ffffff',
+    fullBlack: '#000000',
+};
